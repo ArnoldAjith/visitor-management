1327626eb3457c07b8028d6acc18702fd67943fa
Ajith P <ajithpunniaseelan0604@gmail.com>
2026-03-14 22:00:51 +0530

    api gateway

---
+++ api-gateway/src/main/java/com/api/gateway/demo/VisiterGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.api.gateway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisiterGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisiterGatewayApplication.class, args);
+	}
+
+}
