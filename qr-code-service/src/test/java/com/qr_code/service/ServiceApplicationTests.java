0df847612cf96598f057cd3ce0405c15cf40604b
Ajith P <ajithpunniaseelan0604@gmail.com>
2026-03-14 23:22:48 +0530

    all service added

---
+++ qr-code-service/src/test/java/com/qr_code/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qr_code.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ visitor-service/src/test/java/com/visitor/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visitor.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
